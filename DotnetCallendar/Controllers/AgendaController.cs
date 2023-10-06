0c53099ff75713e4d2c8a971d5364620c9b81d66
RodrigoPelais <rpelais52@gmail.com>
2023-10-06 16:23:53 -0300

    agenda rodando

--- DotnetCallendar/Controllers/AgendaController.cs
+++ DotnetCallendar/Controllers/AgendaController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotnetCallendar.Data;
+using DotNetCoreCalendar.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
-namespace DotnetCallendar.Controllers
+namespace DotnetCallendar.Controllers;
+
+public class AgendaController : Controller
 {
-    public class AgendaController : Controller
+
+    private readonly ApplicationDbContext _context;
+
+    public AgendaController(ApplicationDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+
+        var localizacao=_context.Locais.ToList();
+        var eventos=_context.Eventos.ToList();  
+
+        ViewData["Resources"] = JSONListHelper.GetResourceListJSONString(localizacao);
+        ViewData["Events"] = JSONListHelper.GetEventListJSONString(eventos);
+
+        return View();
     }
 }
---
+++ DotnetCallendar/Helpers/JSONListHelper.cs
@@ -0,0 +1,68 @@
+﻿using DotnetCallendar.Models.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCalendar.Helpers
+{
+    public static class JSONListHelper
+    {
+        public static string GetEventListJSONString(List<Evento> events)
+        {
+            var listaEvento = new List<Event>();
+
+            foreach (var model in events)
+            {
+                var evento = new Event()
+                {
+                    id = model.Id,
+                    
+                    dataInicio = model.DataInicio,  
+                    dataFim = model.DataFim,
+                    descricao = model.Descricao,    
+                    //resourceId = model.Location.Id,
+                    nome = model.Nome
+                };
+                listaEvento.Add(evento);
+            }
+            return System.Text.Json.JsonSerializer.Serialize(listaEvento);
+        }
+
+        public static string GetResourceListJSONString(List<Localizacao> locations)
+        {
+            var lista = new List<Resource>();
+
+            foreach (var loc in locations)
+            {
+                var resource = new Resource()
+                {
+                    id = loc.Id,
+                    nome = loc.Nome
+                };
+                lista.Add(resource);
+            }
+            return System.Text.Json.JsonSerializer.Serialize(lista);
+        }
+    }
+
+    public class Event
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public DateTime dataInicio { get; set; }
+        public DateTime dataFim { get; set; }
+        public int resourceId { get; set; }
+
+    }
+
+
+    public class Resource
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+    }
+}
